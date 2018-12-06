cfeed4c10946168203b939352b48115940ca19bc
Nishant Kumar <niskumar@adobe.com>
2018-12-06 17:58:29 +0530

    Adding changes for face landmark detection

---
+++ facialLandmarkDetection.h
@@ -0,0 +1,14 @@
+//
+//  facialLandmarkDetection.h
+//  HelloCV
+//
+//  Created by Nishant Kumar on 04/11/18.
+//  Copyright © 2018 HelloCv. All rights reserved.
+//
+
+#ifndef facialLandmarkDetection_h
+#define facialLandmarkDetection_h
+
+int detectFaceLandmarks();
+
+#endif /* facialLandmarkDetection_h */
